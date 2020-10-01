2a724ed1af79de519fb332853ff324f0d449ea97
Lemeshonok Kirill <lemeshonok12@gmail.com>
2020-10-01 21:13:59 +0700

    add hover effects for dish likes

--- src/components/MenuDish/DishLikes/index.tsx
+++ src/components/MenuDish/DishLikes/index.tsx
@@ -18,11 +18,11 @@ interface IDishLikesProps {
 export const DishLikes: React.FC<IDishLikesProps> = ({ className, likes, disabled, onLike, onDislike }) => {
   return (
     <div className={cn('d-flex align-items-center dish__likes', className)}>
-      <div>
+      <div className={cn({ 'dish__like-wrapper': !disabled })}>
         <LikeIcon className={cn({ 'cursor-pointer': !disabled })} onClick={!disabled ? onLike : undefined} />
         <span className="ml-1">{likes.likeCount}</span>
       </div>
-      <div className="ml-2">
+      <div className={cn({ 'dish__dislike-wrapper': !disabled }, 'ml-2')}>
         <DislikeIcon className={cn({ 'cursor-pointer': !disabled })} onClick={!disabled ? onDislike : undefined} />
         <span className="ml-1">{likes.dislikeCount}</span>
       </div>
